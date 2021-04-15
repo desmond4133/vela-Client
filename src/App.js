ebec89e201c14a815b92d3b1d711bb2e9d972690
duna2 <36515780+duna2@users.noreply.github.com>
2021-04-15 20:58:55 +0100

    added Nav component with styling and link path

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './booking/Home';
 import Login from './auth/Login';
 import Register from './auth/Register';
+import Link from 'react-router-dom';
+
+
+const TopNav = () => (
+  <div className="nav big-light d-flex justify-content-between">
+    <link className="nav-link" to="/">Home</link>
+  </div>
+)
+
 
 function App() {
   return (

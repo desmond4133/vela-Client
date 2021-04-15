d4e4f4e3959f1e658d021b68127446572ba352ab
duna2 <36515780+duna2@users.noreply.github.com>
2021-04-15 20:07:32 +0100

    installed react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './booking/Home';
 import Login from './auth/Login';
 import Register from './auth/Register';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-      <Login/>
-      <Register/>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
